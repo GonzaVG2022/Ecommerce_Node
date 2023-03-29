29da98c76807ca12b0bc51008c514b51953c445e
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2023-03-28 21:50:14 -0300

    cloudinari

--- src/routes/productImg.router.js
+++ src/routes/productImg.router.js
@@ -3,12 +3,14 @@ const express = require('express');
 const verifyJWT = require('../utils/verifyJWT');
 const upload = require('../utils/multer');
 
+
 const productImgRouter = express.Router();
 
+    
 productImgRouter.route('/')
     .get(verifyJWT, getAll)
     .post(verifyJWT, upload.single('image'), create);
-
+    
 productImgRouter.route('/:id')
     .get(getOne)
     .delete(verifyJWT, remove)
